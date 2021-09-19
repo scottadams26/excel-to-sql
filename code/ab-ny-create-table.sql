58bed0a329ca48101f739cdf436c982ff3227eb2
Scott Adams <scottadams26@gmail.com>
2021-09-19 10:43:42 -0500

    Initial commit of code folder

---
+++ code/ab-ny-create-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ab-ny (
+        id TEXT NOT NULL,
+        name TEXT NOT NULL,
+        host_id TEXT NOT NULL,
+        neighbourhood_group TEXT,
+        neighbourhood TEXT,
+        room_type TEXT,
+        price FLOAT,
+        number_of_reviews INTEGER,
+        last_review DATE,
+        reviews_per_month FLOAT
+ ); 
+        
